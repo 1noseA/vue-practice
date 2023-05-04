b14ca5bd04ae811b65bf955d5f94840acaccb8cc
1noseA <1noseA2L@gmail.com>
2023-05-04 12:23:57 +0900

    新規作成画面追加

---
+++ src/views/NewView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div><input type="text"></div>
+    <div><textarea></textarea></div>
+    <div class="center">
+      <button>保存</button>
+    </div>
+  </div>
+</template>
+
+<!-- scopedを入れるとそのファイルだけstyleを当てることができる -->
+<style scoped>
+  div {
+    margin-bottom: 10px;
+  }
+  input[type=text] {
+    width: 100%;
+  }
+  textarea {
+    width: 100%;
+    height: 30em;
+  }
+  button {
+    width: 5em;
+    margin: 3px;
+  }
+  .center {
+    text-align: center;
+  }
+</style>
