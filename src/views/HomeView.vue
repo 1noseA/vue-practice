2fd2c9f0890894c4014912d7c94efca2a0843d02
1noseA <1noseA2L@gmail.com>
2023-05-04 14:44:50 +0900

    一覧画面作成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-if="hasCards">
+      <li v-for="card in cards" :key="card.id">
+        {{ card.word }}
+      </li>
+    </ul>
+    <p v-else>単語はありません</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  // storeのデータを渡す
+  computed: {
+    hasCards() {
+      return this.$store.state.cards.length
+    },
+    cards() {
+      return this.$store.state.cards
+    }
   }
 }
 </script>
